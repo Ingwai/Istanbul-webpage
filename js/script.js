6386c746f7ae62a7530f7ca91e2c043165f0d215
Ingwai <ingwai@wp.pl>
2022-08-11 15:57:38 +0200

    first section - hero - add changing bcg images

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 const title = document.querySelectorAll('.title path');
 
 title.forEach((letter, index) => console.log(`letter of ${index} is ${letter.getTotalLength()}`));
+
+const firstBg = document.querySelector('.first-bg');
+
+function opacity(timeAnim) {
+	setInterval(() => {
+		firstBg.classList.toggle('bg-opacity');
+		firstBg.style.transition = 'opacity 3s';
+	}, timeAnim * 1000);
+}
+
+opacity(15);
