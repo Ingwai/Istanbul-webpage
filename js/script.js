b5f6d243f9ceab3d1bd2e4f025ce923a8ac27b79
Ingwai <ingwai@wp.pl>
2022-08-11 20:23:19 +0200

    fixed name of class for BEM

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const title = document.querySelectorAll('.title path');
 
 title.forEach((letter, index) => console.log(`letter of ${index} is ${letter.getTotalLength()}`));
 
-const firstBg = document.querySelector('.first-bg');
+const firstBg = document.querySelector('.header__bg-1');
 
 function opacity(timeAnim) {
 	setInterval(() => {
-		firstBg.classList.toggle('bg-opacity');
+		firstBg.classList.toggle('header__bg-opacity');
 		firstBg.style.transition = 'opacity 3s';
 	}, timeAnim * 1000);
 }
 
-opacity(15);
+opacity(12);
